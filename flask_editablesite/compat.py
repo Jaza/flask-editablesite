b68214ac8062af70db608bd4f8aca9e566a8951b
Jeremy Epstein <jazepstein@gmail.com>
2015-10-21 13:08:30 +1100

    make flake8 on py3.x happy

--- flask_editablesite/compat.py
+++ flask_editablesite/compat.py
@@ -7,11 +7,11 @@ import sys
 PY2 = int(sys.version[0]) == 2
 
 if PY2:
-    text_type = unicode
+    text_type = unicode  # flake8: noqa
     binary_type = str
-    string_types = (str, unicode)
-    unicode = unicode
-    basestring = basestring
+    string_types = (str, unicode)  # flake8: noqa
+    unicode = unicode  # flake8: noqa
+    basestring = basestring  # flake8: noqa
 else:
     text_type = str
     binary_type = bytes

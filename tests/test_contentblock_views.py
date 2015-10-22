b7cbb6a9ec28a0feb67c77ad637fc4e4e9fa1c1f
Jeremy Epstein <jazepstein@gmail.com>
2015-10-22 11:57:59 +1100

    add some more tests of default content

--- tests/test_contentblock_views.py
+++ tests/test_contentblock_views.py
@@ -3,11 +3,14 @@
 
 import pytest
 
+from flask import url_for
+
+from flask_editablesite.extensions import thumb
+from flask_editablesite.contentblock.models import (
+    ShortTextContentBlock, RichTextContentBlock)
+
 
 def test_home_textcontentblock_default_content_visible(db, testapp):
-    from flask_editablesite.contentblock.models import (
-        ShortTextContentBlock, RichTextContentBlock)
-
     default_content = ShortTextContentBlock.default_content()
     default_content.update(
         RichTextContentBlock.default_content())
@@ -16,3 +19,20 @@ def test_home_textcontentblock_default_content_visible(db, testapp):
 
     for o in default_content.values():
         assert o.content in res.text
+
+
+def test_home_imagecontentblock_default_site_logo_visible(app, db,
+                                                          testapp):
+    res = testapp.get("/")
+
+    assert (
+        (
+            '<img class="img-responsive img-circle img-lesswidth" '
+            'src="{0}" alt="{1}">').format(
+                url_for('static', filename=thumb.thumbnail(
+                    app.config[
+                        'EDITABLE_PLACEHOLDER_IMAGE_RELATIVE_PATH'],
+                    size='256x256',
+                    crop='fit')),
+                app.config['SITE_NAME'])
+        in res.text)
--- tests/test_sample_text.py
+++ tests/test_sample_text.py
@@ -4,6 +4,7 @@
 import pytest
 
 from flask_editablesite.app import create_app
+from flask_editablesite.settings import TestConfig
 
 
 from flask_editablesite.editable.sample_text import (
@@ -13,3 +14,34 @@ from flask_editablesite.editable.sample_text import (
 def test_placeholder_when_no_scrape_configured(app):
     sample_text = placeholder_or_random_sample_text()
     assert sample_text == app.config['EDITABLE_PLACEHOLDER_TEXT']
+
+
+class TextScrapeTestConfig(TestConfig):
+    EDITABLE_SAMPLE_TEXT_SCRAPE_URLS = [
+        (
+            'https://raw.githubusercontent.com'
+            '/Jaza/flask-editablesite'
+            '/master/tests/sample-text-great-expectations.txt'),]
+
+
+def test_markovify_when_scrape_configured():
+    app = create_app(TextScrapeTestConfig)
+
+    with app.app_context():
+        sample_text = placeholder_or_random_sample_text()
+
+        assert sample_text is not None
+        assert len(sample_text)
+        assert sample_text != app.config['EDITABLE_PLACEHOLDER_TEXT']
+
+        assert (
+            ('and' in sample_text)
+            or ('the' in sample_text)
+            or ('that' in sample_text)
+            or ('you' in sample_text)
+            or ('man' in sample_text)
+            or ('was' in sample_text)
+            or ('with' in sample_text)
+            or ('said' in sample_text)
+            or ('him' in sample_text)
+            or ('were' in sample_text))
